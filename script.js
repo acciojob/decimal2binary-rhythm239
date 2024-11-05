deb1907c5e5c32a3635bb6096c218b7b92733bf1
ridham <134303835+rhythm239@users.noreply.github.com>
2024-11-05 18:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+    // Edge case: If the input number is 0, return "0"
+    if (num === 0) return "0";
+    
+    let binary = "";
+    
+    // Continue dividing by 2 and storing remainders until num becomes 0
+    while (num > 0) {
+        binary = (num % 2) + binary;  // Append remainder at the beginning of the binary string
+        num = Math.floor(num / 2);    // Update num to be half of its previous value
+    }
+    
+    return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
